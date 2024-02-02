538b94802828245357c01e9fca28f9a7e4e4a008
AlmaDuranHernandez <152935309+AlmaDuranHernandez@users.noreply.github.com>
2024-02-02 10:02:47 -0600

    cambios login

--- VISTAS/Login.php
+++ VISTAS/Login.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>LOGIN</title>
-        <link rel="stylesheet" href="../Publico/CSS/estilos.css">
+        <link rel="stylesheet" href="../GLOBAL/CSS/login.css">
   </head>
   <body>
 
