e9d3b018c964f2108ce7f32ac56171a8954fc0f1
DaniDoom35 <125550224+DaniDoom35@users.noreply.github.com>
2024-02-13 14:59:42 -0600

    Prueba para modelo
    Se realizo un ejemplo de consulta

--- MODELO/modelo.php
+++ MODELO/modelo.php
@@ -7,9 +7,25 @@ include('conexion.php');
 class Modelo {
 
     public function obtenerInformacion() {
-        global $conn; // Accede a la variable $conn definida en conexion.php
+        global $conn; 
 
-        // Ejemplo de consulta SQL
+
+        $sql = "SELECT * FROM departamento";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"]. " " . $row["fecha"]. "<br>";
+            }
+        } else {
+            echo "0 Resultados";
+        }
+
+
+        $conn->close();            
+
         
     }
 }
--- VISTAS/Cliente.php
+++ VISTAS/Cliente.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cliente</title>
-    <link rel="stylesheet" href="Cliente.css">
+    <link rel="stylesheet" href="../GLOBAL/CSS/cliente.css">
 
 </head>
 <body>
